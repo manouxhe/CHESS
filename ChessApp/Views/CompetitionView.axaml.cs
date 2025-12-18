45f327160b69652ccfab11b4aef450b67ef4e208
Manar El Kadi <23346@ecam.be>
2025-12-18 18:41:24 +0100

    Correction soucies liés à l'interface

---
+++ ChessApp/Views/CompetitionView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ChessApp.Views;
+
+public partial class CompetitionView : UserControl
+{
+    public CompetitionView()
+    {
+        InitializeComponent();
+    }
+}
